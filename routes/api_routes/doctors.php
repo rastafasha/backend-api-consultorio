6461d29d74b1380db0bc4e6b853d505cb29ecda4
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-02-22 22:11:52 -0400

    update rutas

--- routes/api_routes/doctors.php
+++ routes/api_routes/doctors.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\Admin\Doctor\DoctorController;
 
 
 Route::get('doctors', [DoctorController::class, 'index'])->name('doctor.index');
-Route::get('doctor/config', [DoctorController::class, 'config'])->name('doctor.config');
-Route::post('doctor/store', [DoctorController::class, 'store'])->name('doctor.store');
-Route::get('doctor/show/{id}', [DoctorController::class, 'show'])->name('doctor.show');
-Route::post('doctor/update/{id}', [DoctorController::class, 'update'])->name('doctor.update');
-Route::delete('doctor/destroy/{id}', [DoctorController::class, 'destroy'])->name('doctor.destroy');
+Route::get('doctors/config', [DoctorController::class, 'config'])->name('doctor.config');
+Route::post('doctors/store', [DoctorController::class, 'store'])->name('doctor.store');
+Route::get('doctors/show/{id}', [DoctorController::class, 'show'])->name('doctor.show');
+Route::post('doctors/update/{id}', [DoctorController::class, 'update'])->name('doctor.update');
+Route::delete('doctors/destroy/{id}', [DoctorController::class, 'destroy'])->name('doctor.destroy');
 Route::put('/doctorupdate/status/{id}', [DoctorController::class, 'updateStatus'])
     ->name('doctor.updateStatus');
-Route::get('doctor/profile/{id}', [DoctorController::class, 'profile'])->name('doctor.profile');
+Route::get('doctors/profile/{id}', [DoctorController::class, 'profile'])->name('doctor.profile');
 
--- routes/api_routes/staff.php
+++ routes/api_routes/staff.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Admin\Staff\StaffsController;
 
 
 Route::get('staffs', [StaffsController::class, 'index'])->name('staff.index');
-Route::get('staff/config', [StaffsController::class, 'config'])->name('staff.config');
-Route::post('staff/store', [StaffsController::class, 'store'])->name('staff.store');
-Route::get('staff/show/{id}', [StaffsController::class, 'show'])->name('staff.show');
-Route::post('staff/update/{role}', [StaffsController::class, 'update'])->name('staff.update');
-Route::delete('staff/destroy/{id}', [StaffsController::class, 'destroy'])->name('staff.destroy');
+Route::get('staffs/config', [StaffsController::class, 'config'])->name('staff.config');
+Route::post('staffs/store', [StaffsController::class, 'store'])->name('staff.store');
+Route::get('staffs/show/{id}', [StaffsController::class, 'show'])->name('staff.show');
+Route::post('staffs/update/{role}', [StaffsController::class, 'update'])->name('staff.update');
+Route::delete('staffs/destroy/{id}', [StaffsController::class, 'destroy'])->name('staff.destroy');
 
