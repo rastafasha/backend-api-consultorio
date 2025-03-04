d158edf87d57b085123dac857d03102ddbaadde5
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-03-04 10:24:55 -0400

    Add doctor schedule days seeder with user role assignment

--- database/seeders/DoctorScheduleDaySeeder.php
+++ database/seeders/DoctorScheduleDaySeeder.php
@@ -15,6 +15,77 @@ class DoctorScheduleDaySeeder extends Seeder
      */
     public function run()
     {
+
+        // Get or create a doctor user
+        $doctor = User::whereHas('roles', function($q) {
+            $q->where('name', 'like', '%DOCTOR%');
+        })->first();
+
+        if (!$doctor) {
+            // Create a new doctor user if none exists
+            $doctor = User::factory()->create();
+            $doctorRole = Role::firstOrCreate(['name' => 'DOCTOR']);
+            $doctor->assignRole($doctorRole);
+        }
+
+        $scheduleDays = [
+            [
+                'id' => 1,
+                'user_id' => $doctor->id,
+                'day' => 'Lunes',
+                'created_at' => '2023-12-09 16:13:30',
+                'updated_at' => '2023-12-09 16:13:30',
+                'deleted_at' => null
+            ],
+            [
+                'id' => 2,
+                'user_id' => $doctor->id,
+                'day' => 'Lunes',
+                'created_at' => '2023-12-10 01:35:40',
+                'updated_at' => '2023-12-10 01:35:40',
+                'deleted_at' => null
+            ],
+            [
+                'id' => 3,
+                'user_id' => $doctor->id,
+                'day' => 'Martes',
+                'created_at' => '2023-12-10 01:35:40',
+                'updated_at' => '2023-12-10 01:35:40',
+                'deleted_at' => null
+            ],
+            [
+                'id' => 4,
+                'user_id' => $doctor->id,
+                'day' => 'Miercoles',
+                'created_at' => '2023-12-10 01:35:40',
+                'updated_at' => '2023-12-10 01:35:40',
+                'deleted_at' => null
+            ],
+            [
+                'id' => 5,
+                'user_id' => $doctor->id,
+                'day' => 'Jueves',
+                'created_at' => '2023-12-10 01:35:40',
+                'updated_at' => '2023-12-10 01:35:40',
+                'deleted_at' => null
+            ],
+            [
+                'id' => 6,
+                'user_id' => $doctor->id,
+                'day' => 'Viernes',
+                'created_at' => '2023-12-10 01:35:40',
+                'updated_at' => '2023-12-10 01:35:40',
+                'deleted_at' => null
+            ],
+            [
+                'id' => 7,
+                'user_id' => $doctor->id,
+                'day' => 'Lunes',
+                'created_at' => '2023-12-10 01:37:54',
+                'updated_at' => '2023-12-10 01:37:54',
+                'deleted_at' => null
+            ]
+        ];
         // Create sample doctor schedule days
         $doctorUser = User::whereHas('roles', function($query) {
             $query->where('name', 'DOCTOR');
