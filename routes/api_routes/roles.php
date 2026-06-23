673f4c04ea55f9b9092171f8f555ba112858343e
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-06-23 18:01:09 -0400

    Fix: Evitar duplicado de rutas usando except en el resource

--- routes/api_routes/roles.php
+++ routes/api_routes/roles.php
@@ -4,9 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Role\RolesController;
 
-Route::resource('roles', RolesController::class);
+// Le indicamos al resource que ignore los métodos que tú ya hiciste a mano
+Route::resource('roles', RolesController::class)->except([
+    'store', 'show', 'update', 'destroy'
+]);
+
 Route::post('roles/store', [RolesController::class, 'roleStore'])->name('roles.store');
 Route::get('roles/show/{role}', [RolesController::class, 'roleShow'])->name('roles.show');
 Route::put('roles/update/{role}', [RolesController::class, 'roleUpdate'])->name('roles.update');
 Route::delete('roles/destroy/{role}', [RolesController::class, 'roleDestroy'])->name('roles.destroy');
-
