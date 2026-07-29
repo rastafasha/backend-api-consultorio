2dd15d75516f62b0b1abfcbb0edf728d22aa5943
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-07-29 19:18:04 -0400

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'http://localhost:4200'],
+    'allowed_origins' => ['https://consultorio.klyntic.com', 'https://pconsultorio.klyntic.com'],
 
     'allowed_origins_patterns' => ['*'],
 
