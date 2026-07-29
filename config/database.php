b5c1953c5cfd1f39eff31ef49430158efe5a2cbd
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-07-29 13:21:51 -0400

    fix config database1

--- config/database.php
+++ config/database.php
@@ -65,19 +65,19 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => null, 
+            'host' => env('DB_HOST', 'aws-0-ca-central-1.pooler.supabase.com'),
+            'port' => env('DB_PORT', '6543'),
+            'database' => env('DB_DATABASE', 'postgres'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'hVovCluMixG2OqrK'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
             'options' => [
-                PDO::ATTR_EMULATE_PREPARES => true, // Emula sentencias preparadas a nivel local en PHP
+                PDO::ATTR_EMULATE_PREPARES => true,
             ],
         ],
 
