ee3df4d7111765c6e3b8437f507a27625b0b96a1
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-07-30 09:22:04 -0400

    fix role routes

--- routes/api_routes/roles.php
+++ routes/api_routes/roles.php
@@ -4,11 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Role\RolesController;
 
-// Le indicamos al resource que ignore los métodos que tú ya hiciste a mano
-Route::resource('roles', RolesController::class)->except([
-    'store', 'show', 'update', 'destroy'
-]);
+// 🛠️ FIX DE RUTAS: Declaramos el método GET de forma explícita y directa
+// Esto obliga a Laravel a ignorar cualquier recurso automático y ejecutar estrictamente tu RolesController
+Route::get('roles', [RolesController::class, 'index'])->name('roles.index');
 
+// Tus rutas POST, PUT, DELETE manuales se mantienen perfectas abajo:
 Route::post('roles/store', [RolesController::class, 'roleStore'])->name('roles.store');
 Route::get('roles/show/{role}', [RolesController::class, 'roleShow'])->name('roles.show');
 Route::put('roles/update/{role}', [RolesController::class, 'roleUpdate'])->name('roles.update');
