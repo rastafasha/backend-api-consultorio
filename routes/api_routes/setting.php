107278ec485b3f5bb1530fc7c40ab9f96994ee74
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-07-30 09:25:18 -0400

    fix setting fix

--- routes/api_routes/setting.php
+++ routes/api_routes/setting.php
@@ -4,10 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\SettingGController;
 
-Route::resource('roles', SettingGController::class)->except([
-    'store', 'show', 'update', 'destroy'
-]);
-Route::resource('setting', SettingGController::class);
+Route::get('setting', [SettingGController::class, 'index'])->name('setting.index');
+
 Route::post('setting/store', [SettingGController::class, 'settingStore'])->name('setting.settingStore');
 Route::get('setting/show/{id}', [SettingGController::class, 'settingShow'])->name('setting.settingShow');
 Route::post('setting/update/{id}', [SettingGController::class, 'settingUpdate'])->name('setting.settingUpdate');
